4f6df6c37e54c8b17aa77f82071ea7f59b33367e
cd <claudio.donofrio@nateko.lu.se>
2022-11-07 22:00:42 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bulkrename',
-    version='0.1.0',
+    version='0.1.1',
 	author="Claudio D'Onofrio",
     author_email='claudio.donofrio@gmail.com', 
     py_modules=['bulkrename'],
